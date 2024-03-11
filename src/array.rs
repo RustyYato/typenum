4b9a80810f944a4a6168168fd59fd59e842614cc
RustyYato <rustyyato@gmail.com>
2024-03-10 21:18:40 -0700

    implement an arrayvec with a perfect length type

--- src/array.rs
+++ src/array.rs
@@ -12,9 +12,14 @@ use super::Unsigned;
 #[repr(transparent)]
 pub struct Array<T, N: Unsigned>(N::Array<T>);
 
-pub struct SimpleArrayVec<T, N: Unsigned> {
+pub struct ArrayBuilder<T, N: Unsigned> {
     array: Array<MaybeUninit<T>, N>,
     len: usize,
+}
+
+pub struct ArrayVec<T, N: ArrayLength> {
+    array: Array<MaybeUninit<T>, N>,
+    len: N::Index,
 }
 
 const unsafe fn transmute_unchecked<T, U>(t: T) -> U {
@@ -36,14 +41,14 @@ const unsafe fn transmute_unchecked<T, U>(t: T) -> U {
     }
 }
 
-impl<T, N: Unsigned> Drop for SimpleArrayVec<T, N> {
+impl<T, N: Unsigned> Drop for ArrayBuilder<T, N> {
     fn drop(&mut self) {
         let ptr = self.array.as_mut_ptr();
         unsafe { ptr::slice_from_raw_parts_mut(ptr, self.len).drop_in_place() }
     }
 }
 
-impl<T, N: Unsigned> SimpleArrayVec<T, N> {
+impl<T, N: Unsigned> ArrayBuilder<T, N> {
     pub const fn new() -> Self {
         Self {
             array: Array::uninit(),
@@ -69,6 +74,63 @@ impl<T, N: Unsigned> SimpleArrayVec<T, N> {
     pub unsafe fn into_array_unchecked(self) -> Array<T, N> {
         let m = ManuallyDrop::new(self);
         unsafe { core::ptr::read(&m.array).assume_init() }
+    }
+}
+
+impl<T, N: ArrayLength> ArrayVec<T, N> {
+    pub const fn new() -> Self {
+        Self {
+            array: Array::uninit(),
+            len: ArrayIndex::ZERO,
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.len.to_usize()
+    }
+
+    pub fn push(&mut self, value: T) {
+        self.array[self.len.to_usize()] = MaybeUninit::new(value);
+        self.len.inc();
+    }
+
+    pub fn try_pop(&mut self) -> Option<T> {
+        if let Some(len) = self.len.checked_dec() {
+            self.len = len;
+            let slot = unsafe { self.array.get_unchecked(len.to_usize()) };
+            Some(unsafe { slot.as_ptr().read() })
+        } else {
+            None
+        }
+    }
+
+    pub fn pop(&mut self) -> T {
+        self.try_pop().expect("Tried to pop from an empty ArrayVec")
+    }
+
+    pub unsafe fn push_unchecked(&mut self, value: T) {
+        *self.array.get_unchecked_mut(self.len.to_usize()) = MaybeUninit::new(value);
+        self.len.inc();
+    }
+
+    pub fn into_array(self) -> Array<T, N> {
+        assert!(self.len.to_usize() == N::TO_USIZE);
+        unsafe { self.into_array_unchecked() }
+    }
+
+    pub unsafe fn into_array_unchecked(self) -> Array<T, N> {
+        let m = ManuallyDrop::new(self);
+        unsafe { core::ptr::read(&m.array).assume_init() }
+    }
+
+    pub fn as_slice(&self) -> &[T] {
+        unsafe { core::slice::from_raw_parts(self.array.as_ptr().cast(), self.len.to_usize()) }
+    }
+
+    pub fn as_slice_mut(&mut self) -> &mut [T] {
+        unsafe {
+            core::slice::from_raw_parts_mut(self.array.as_mut_ptr().cast(), self.len.to_usize())
+        }
     }
 }
 
@@ -211,7 +273,7 @@ impl<T: Hash, N: Unsigned> Hash for Array<T, N> {
 impl<T: Copy, N: Unsigned> Copy for Array<T, N> where N::Array<T>: Copy {}
 impl<T: Clone, N: Unsigned> Clone for Array<T, N> {
     fn clone(&self) -> Self {
-        let mut v = SimpleArrayVec::<T, N>::new();
+        let mut v = ArrayBuilder::<T, N>::new();
 
         for i in self.iter() {
             unsafe { v.push_unchecked(i.clone()) }
@@ -224,5 +286,126 @@ impl<T: Clone, N: Unsigned> Clone for Array<T, N> {
 impl<T: core::fmt::Debug, N: Unsigned> core::fmt::Debug for Array<T, N> {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         self.as_slice().fmt(f)
+    }
+}
+
+pub trait ArrayLength: Unsigned {
+    type Index: ArrayIndex;
+}
+
+#[doc(hidden)]
+pub trait ArrayLengthSelector<FitsU8, FitsU16, FitsU32, FitsU64> {
+    type SelectorIndex: ArrayIndex;
+}
+
+impl<N: Unsigned> ArrayLength for N
+where
+    (): ArrayLengthSelector<
+        super::Cmp<N, super::Pow<super::consts::P2, super::consts::P8>>,
+        super::Cmp<N, super::Pow<super::consts::P2, super::consts::P16>>,
+        super::Cmp<N, super::Pow<super::consts::P2, super::consts::P32>>,
+        super::Cmp<N, super::Pow<super::consts::P2, super::consts::P64>>,
+    >,
+{
+    type Index = <() as ArrayLengthSelector<
+        super::Cmp<N, super::Pow<super::consts::P2, super::consts::P8>>,
+        super::Cmp<N, super::Pow<super::consts::P2, super::consts::P16>>,
+        super::Cmp<N, super::Pow<super::consts::P2, super::consts::P32>>,
+        super::Cmp<N, super::Pow<super::consts::P2, super::consts::P64>>,
+    >>::SelectorIndex;
+}
+
+use super::{cmp::OrdGe, OrdLess};
+
+impl<FitsU16, FitsU32, FitsU64> ArrayLengthSelector<OrdLess, FitsU16, FitsU32, FitsU64> for () {
+    type SelectorIndex = u8;
+}
+
+impl<FitsU8: OrdGe, FitsU32, FitsU64> ArrayLengthSelector<FitsU8, OrdLess, FitsU32, FitsU64>
+    for ()
+{
+    type SelectorIndex = u16;
+}
+
+impl<FitsU8: OrdGe, FitsU16: OrdGe, FitsU64> ArrayLengthSelector<FitsU8, FitsU16, OrdLess, FitsU64>
+    for ()
+{
+    type SelectorIndex = u32;
+}
+
+impl<FitsU8: OrdGe, FitsU16: OrdGe, FitsU32: OrdGe>
+    ArrayLengthSelector<FitsU8, FitsU16, FitsU32, OrdLess> for ()
+{
+    type SelectorIndex = u64;
+}
+
+pub unsafe trait ArrayIndex: Copy {
+    const ZERO: Self;
+
+    fn to_usize(self) -> usize;
+
+    fn inc(&mut self);
+
+    fn checked_dec(self) -> Option<Self>;
+}
+
+unsafe impl ArrayIndex for u8 {
+    const ZERO: Self = 0;
+
+    fn to_usize(self) -> usize {
+        self as usize
+    }
+
+    fn inc(&mut self) {
+        *self += 1;
+    }
+
+    fn checked_dec(self) -> Option<Self> {
+        self.checked_sub(1)
+    }
+}
+unsafe impl ArrayIndex for u16 {
+    const ZERO: Self = 0;
+
+    fn to_usize(self) -> usize {
+        self as usize
+    }
+
+    fn inc(&mut self) {
+        *self += 1;
+    }
+
+    fn checked_dec(self) -> Option<Self> {
+        self.checked_sub(1)
+    }
+}
+unsafe impl ArrayIndex for u32 {
+    const ZERO: Self = 0;
+
+    fn to_usize(self) -> usize {
+        self as usize
+    }
+
+    fn inc(&mut self) {
+        *self += 1;
+    }
+
+    fn checked_dec(self) -> Option<Self> {
+        self.checked_sub(1)
+    }
+}
+unsafe impl ArrayIndex for u64 {
+    const ZERO: Self = 0;
+
+    fn to_usize(self) -> usize {
+        self as usize
+    }
+
+    fn inc(&mut self) {
+        *self += 1;
+    }
+
+    fn checked_dec(self) -> Option<Self> {
+        self.checked_sub(1)
     }
 }
